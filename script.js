763548347d1375053ccfbd619e118eef37db03bd
invoker <mgsaw929@gmail.com>
2023-10-28 01:39:48 +0630

    Finished

--- script.js
+++ script.js
@@ -2,12 +2,35 @@
 const userInputTag = document.querySelector("#fruit");
 const unorderListTag = document.querySelector("ol");
 
-userInputTag.addEventListener("change", addingFruit);
+userInputTag.addEventListener("change", addingFood);
+unorderListTag.addEventListener("click", clickingList);
 
 
-function addingFruit(event) {
-    const listTag = document.createElement("li");
-    listTag.textContent = `${userInputTag.value}`;
-    unorderListTag.append(listTag);
+function addingFood(event) {
+    const items = document.createElement("div");
+    items.classList.add('items');
+    items.textContent = userInputTag.value;
+    items.innerHTML = `
+    <li>${items.textContent}</li>
+    <span class="material-symbols-outlined"> delete </span>
+    `;
+    unorderListTag.append(items);
     userInputTag.value = "";
+}
+
+function clickingList(event) {
+    const targetTag = event.target;
+    switch (event.target.tagName) {
+        case "LI":
+            if (targetTag.classList.contains('purchased'))
+                targetTag.classList.remove('purchased');
+            else
+                targetTag.classList.add('purchased');
+            break;
+        case "SPAN":
+            targetTag.parentElement.remove();
+            break;
+        default:
+            break;
+    }
 }
